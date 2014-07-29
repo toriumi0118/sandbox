e5f5f47c74f40b441cf0e4f45b19917d743212a8
Yusuke Nomura <nomura_y@welmo.co.jp>
2014-07-29 20:08:56 +0900

    Add Apache style access log.

--- central2/src/Main.hs
+++ central2/src/Main.hs
@@ -2,12 +2,14 @@ module Main where
 
 import qualified Controller.Echo as Echo
 import qualified Controller.VersionupResult as VersionupResult
+import Network.Wai.Middleware.RequestLogger (logStdout)
 import Web.Scotty
 
 import Auth (auth)
 
 main :: IO ()
 main = scotty 3000 $ do
+    middleware logStdout
     get "/echo/echo" Echo.echo
     auth post "/postpv/pv" undefined
     auth post "/postresult/result" VersionupResult.result

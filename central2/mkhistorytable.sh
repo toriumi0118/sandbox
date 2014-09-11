5ce9ded8fa7b1e8cb8633cca8fe6251eee253380
Yusuke Nomura <nomura_y@welmo.co.jp>
2014-09-11 16:06:11 +0900

    Add mkhistorytable.sh

---
+++ central2/mkhistorytable.sh
@@ -0,0 +1,125 @@
+./dist/build/createtable/createtable -s office_id \
+	business_time \
+	service_time \
+	vacancy \
+	dementia_business_time \
+	dementia_service_time \
+	dementia_vacancy \
+	office \
+	office_appeal_point \
+	office_contract \
+	office_dementia \
+	office_image_com \
+	office_licence \
+	office_price \
+	office_user_info \
+	rel_acupressure_therapist \
+	rel_add_fee \
+	rel_add_fee_help_worker \
+	rel_add_fee_precaution \
+	rel_add_fee_worker \
+	rel_alcohol \
+	rel_architecture \
+	rel_attendant_bed \
+	rel_bath_jet \
+	rel_bath_machine \
+	rel_bath_micro_bubble \
+	rel_bath_onsen \
+	rel_bath_same_sex \
+	rel_bath_time \
+	rel_bath_type \
+	rel_bath_way \
+	rel_big_tv \
+	rel_brain_training \
+	rel_business_kind \
+	rel_car_logo \
+	rel_dementia_add_fee \
+	rel_dementia_add_fee_help_worker \
+	rel_dementia_add_fee_precaution \
+	rel_dementia_add_fee_worker \
+	rel_dementia_ending_course \
+	rel_dementia_floor \
+	rel_dementia_help_level \
+	rel_dementia_judge \
+	rel_dementia_study \
+	rel_dementia_type \
+	rel_design \
+	rel_dose_manage \
+	rel_drive_range \
+	rel_drive_time \
+	rel_elevator \
+	rel_experience_price \
+	rel_extra \
+	rel_facility \
+	rel_female_day \
+	rel_finger_translator \
+	rel_floor_cnt \
+	rel_health_machine \
+	rel_help_level \
+	rel_hiyari \
+	rel_hotline \
+	rel_info_discovery \
+	rel_land_class \
+	rel_local_communication \
+	rel_male_day \
+	rel_massage \
+	rel_meal_breakfast \
+	rel_meal_buffet \
+	rel_meal_dietician \
+	rel_meal_dinner \
+	rel_meal_drink \
+	rel_meal_kampo \
+	rel_meal_launch \
+	rel_meal_made_in \
+	rel_meal_menu \
+	rel_meal_out \
+	rel_meal_place \
+	rel_meal_snack \
+	rel_meal_start \
+	rel_medical \
+	rel_medical_day \
+	rel_midnight_meal \
+	rel_newspaper \
+	rel_night \
+	rel_nurse_call \
+	rel_oral_care \
+	rel_oriental_medicine \
+	rel_pee_same_sex \
+	rel_pet \
+	rel_recreation \
+	rel_rehab_machine \
+	rel_remodeling \
+	rel_scale \
+	rel_self_out \
+	rel_single_care \
+	rel_smoking \
+	rel_social_aid \
+	rel_speech_therapist \
+	rel_special_field \
+	rel_stay_night \
+	rel_super_player \
+	rel_terminal_care \
+	rel_user_tablet \
+	rel_violence \
+	rel_volunteer \
+	rel_wheelchair \
+	rel_with_building \
+	rel_with_pet
+
+./dist/build/createtable/createtable -s office_id \
+	kyotaku \
+	kyotaku_license \
+	rel_kyotaku_service_area \
+	rel_kyotaku_care_add_fee_staff \
+	rel_kyotaku_care_add_fee_service \
+	rel_kyotaku_care_add_fee_other \
+	rel_kyotaku_tell_emergency \
+	kyotaku_business_time
+
+./dist/build/createtable/createtable -s id office_pdf
+
+./dist/build/createtable/createtable -s office_pdf_id office_case_rel
+
+#./dist/build/createtable/createtable -s id news_head
+
+./dist/build/createtable/createtable -s id topic

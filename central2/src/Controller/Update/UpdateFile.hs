f19d9f47bdb50e3fdf4ddb32482d4a0263ee534e
Yusuke Nomura <nomura_y@welmo.co.jp>
2014-09-19 15:51:45 +0900

    Add OfficeImgUpdateProvider.

--- central2/src/Controller/Update/UpdateFile.hs
+++ central2/src/Controller/Update/UpdateFile.hs
@@ -3,32 +3,43 @@ module Controller.Update.UpdateFile
     ) where
 
 import Control.Applicative
+import Control.Monad (unless)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Maybe (catMaybes)
+import System.Directory (doesFileExist)
 import Text.Printf (printf)
 
-import Controller.Update.DataProvider (DataProvider, getHistories, UpdateContent(UpdateOfficeFile), FileType(IMAGE))
-import Controller.Update.HistoryContext (History(History, FileHistory), FileAction(DELETE), targetId)
+import Controller.Update.DataProvider (DataProvider, getHistories, UpdateContent(UpdateOfficeFile), FileType(IMAGE), HistoryId, store)
+import Controller.Update.HistoryContext (History(History, FileHistory, hfFileName), FileAction(DELETE), targetId)
 
-createUpdateContent :: String -> History -> Maybe UpdateContent
-createUpdateContent path (FileHistory _ i a n) = Just $
-    UpdateOfficeFile n IMAGE a (fromIntegral i) path
+createUpdateContent :: FilePath -> History -> Maybe (HistoryId, UpdateContent)
+createUpdateContent path (FileHistory hid i a n) = Just (hid,
+    UpdateOfficeFile n IMAGE a (fromIntegral i) path)
 createUpdateContent _ _ = Nothing
 
-deleteAction :: String -> [History] -> ([History], [UpdateContent])
-deleteAction path = go [] []
+deleteAction
+    :: [(FilePath, History)] -> ([(FilePath, History)], [(HistoryId, UpdateContent)])
+deleteAction = go [] []
   where
-    go rs cs []                                = (rs, reverse $ catMaybes cs)
-    go rs cs (History _ _ _:hs)                = go rs cs hs
-    go rs cs (h@(FileHistory _ _ DELETE _):hs) =
+    go rs cs [] = (rs, reverse $ catMaybes cs)
+    go rs cs ((_, History _ _ _):hs)                   = go rs cs hs
+    go rs cs ((path, h@(FileHistory _ _ DELETE _)):hs) =
         go rs (createUpdateContent path h:cs) hs
-    go rs cs _                                 = undefined
+    go rs cs (h:hs)                                    = go (h:rs) cs hs
 
-updatedFile :: (Functor m, MonadIO m)--, FromSql SqlValue a, ToJSON a)
-    => String -> DataProvider m ()
-updatedFile path = do
-    (hs, uc) <- deleteAction path <$> getHistories
-    undefined
+checkFiles :: MonadIO m
+    => (FilePath, History) -> m ()
+checkFiles (subDir, his) = do
+    b <- liftIO $ doesFileExist file
+    unless b $ fail $ "Update file not found: " ++ file
   where
-    subDirs :: [History] -> [String]
-    subDirs =  map $ printf path . targetId
+    file = subDir ++ "/" ++ hfFileName his
+
+updatedFile :: (Functor m, MonadIO m) => String -> DataProvider m ()
+updatedFile pathFmt = do
+    (hs, uc) <- deleteAction . map ((,) <$> subDir <*> id) <$> getHistories
+    store uc
+    mapM_ checkFiles hs
+    store $ catMaybes $ map (uncurry createUpdateContent) hs
+  where
+    subDir =  printf pathFmt . targetId
